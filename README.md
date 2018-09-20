5554559f47ff2d74ee9d21c1b9395c2449aa4f80
Nam Dau <namdq.nu@gmail.com>
2018-09-20 14:41:47 +0700

    Update for Ghost 2.1.0 compatibility

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ I'm using this for my [blog](http://namdau.com/).
 ![Journal](screenshot.png)
 
 ## Getting Started
+
 You'll need to have `npm` (bundled with [node](https://nodejs.org/)) and [gulp](http://gulpjs.com/) installed and follow these steps to clone the repository, install all necessary packages and build the assets:
 
 
@@ -17,7 +18,9 @@ You'll need to have `npm` (bundled with [node](https://nodejs.org/)) and [gulp](
 
 
 ## Configuration
+
 ### Social links
+
 Open `partials/social.hbs` and replace these with your social links
 
 	<li><a href="#"><i class="fa fa-lg fa-github"></i></a></li>
@@ -25,6 +28,7 @@ Open `partials/social.hbs` and replace these with your social links
 	<li><a href="#"><i class="fa fa-lg fa-linkedin"></i></a></li>
 
 ### External links
+
 Open `partials/external.hbs` and replace these with your external links
 
 	<li><a href="#">External link #1</a></li>
@@ -32,28 +36,32 @@ Open `partials/external.hbs` and replace these with your external links
 	<li><a href="#">External link #3</a></li>
 
 ### Google Analytics
+
 Open `default.hbs` and find this line
 
-	ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
 
 Replace `UA-XXXXX-X` with your Google Analytics ID
 
 ### Disqus
+
 Open `post.hbs` and find this line
 
-	var disqus_shortname = 'example_shortname';
+    var disqus_shortname = 'example_shortname';
 
 Replace `example_shortname` with your Disqus shortname
 
 ## Development
+
 **Journal** uses [gulp](http://gulpjs.com/) as build system and [browser-sync](https://www.browsersync.io) to support synchronised browser testing and live reload, all assets are located at `src` directory and build output is set to default `assets` directory.
 
 Fire up your terminal and run these commands before you start making changes:
 
-	cd /your-ghost-blog/content/themes/journal
-	npm install
-	gulp
+    cd /your-ghost-blog/content/themes/journal
+    npm install
+    gulp
 
 
 ## Copyright & License
+
 [MIT license](LICENSE)
--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,104 @@
-require('es6-promise').polyfill();
+var gulp = require('gulp')
 
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache'),
-    cleanCSS = require('gulp-clean-css'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    size = require('gulp-size'),
-    zip = require('gulp-zip');
-    
+// gulp plugins and utils
+var gutil = require('gulp-util')
+var postcss = require('gulp-postcss')
+var sourcemaps = require('gulp-sourcemaps')
+var zip = require('gulp-zip')
+var rename = require('gulp-rename')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var imagemin = require('gulp-imagemin')
+var cache = require('gulp-cache')
+var browserSync = require('browser-sync').create()
 
-var config = {
+// postcss plugins
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
+var easyimport = require('postcss-easy-import')
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
     proxy: 'localhost:2368',
     open: false,
     injectChanges: true,
-    files: ["assets/styles/*.css", "assets/scripts/*.js"]
-};
-
-gulp.task('browser-sync', function() {
-    browserSync(config);
-});
-
-gulp.task('images', function() {
-    gulp.src('src/images/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('assets/images/'));
-});
-
-gulp.task('styles', function() {
-    return gulp.src('src/styles/**/*.scss')
-        .pipe(plumber({
-            errorHandler: function(error) {
-                console.log(error.message);
-                this.emit('end');
-            }
-        }))
-        .pipe(sass({
-            includePaths: require('node-normalize-scss').includePaths
-        }))
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('assets/styles/'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('assets/styles/'));
-});
-
-gulp.task('scripts', function() {
-    return gulp.src('src/scripts/**/*.js')
-        .pipe(plumber({
-            errorHandler: function(error) {
-                console.log(error.message);
-                this.emit('end');
-            }
-        }))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('assets/scripts/'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('assets/scripts/'));
-});
-
-gulp.task('fonts', function() {
-    return gulp.src('./node_modules/font-awesome/fonts/*.*').pipe(gulp.dest('assets/fonts/'))
-});
-
-// Static Server + watching scss/html files
-gulp.task('default', ['styles', 'scripts', 'fonts', 'images', 'browser-sync'], function() {
-    gulp.watch("src/styles/**/*.scss", ['styles']);
-    gulp.watch("src/scripts/**/*.js", ['scripts']);
-    gulp.watch(config.nodeDir + '/font-awesome/fonts/*', ['fonts']);
-    gulp.watch("*.hbs").on('change', browserSync.reload);
-});
-
-gulp.task('zip', function(){
-    gulp.src(['./**', '!node_modules', '!node_modules/**', '!zip', '!zip/**'])
-    .pipe(zip('built.zip'))
-    .pipe(gulp.dest('zip'))
+    files: ['assets/styles/*.css', 'assets/scripts/*.js'],
+  })
 })
 
-gulp.task('build', ['styles', 'scripts', 'images', 'fonts', 'zip'], function() {
-    return gulp.src(['src/**/*.*']).pipe(size({
-        title: 'build',
-        gzip: true
-    }));
-});
+var swallowError = function swallowError(error) {
+  gutil.log(error.toString())
+  gutil.beep()
+  this.emit('end')
+}
+
+gulp.task('images', function() {
+  gulp
+    .src('src/images/**/*')
+    .pipe(
+      cache(
+        imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }),
+      ),
+    )
+    .pipe(gulp.dest('assets/built/images'))
+})
+
+gulp.task('css', function() {
+  var processors = [
+    easyimport,
+    autoprefixer(['last 2 version']),
+    cssnano({
+      discardComments: { removeAll: true },
+      mergeidents: true,
+      discardDuplicates: true,
+      discardEmpty: true,
+      mergeRules: true,
+      mergeLonghand: true,
+      minifyFontValues: true,
+      normalizeWhitespace: true,
+      reduceInitial: true,
+      reduceTransforms: true,
+      uniqueSelectors: true,
+    }),
+  ]
+
+  return gulp
+    .src('assets/styles/main.css')
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+})
+
+gulp.task('js', function() {
+  return gulp
+    .src('assets/scripts/**/*.js')
+    .on('error', swallowError)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/built/'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('assets/styles/**/*.css', ['css'])
+  gulp.watch('assets/scripts/**/*.js', ['js'])
+  gulp.watch('*.hbs').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['css', 'js', 'images', 'browser-sync'], function() {
+  gulp.start('watch')
+})
+
+gulp.task('zip', ['css', 'js', 'images'], function() {
+  var targetDir = 'dist/'
+  var themeName = require('./package.json').name
+  var filename = themeName + '.zip'
+
+  return gulp
+    .src(['**', '!node_modules', '!node_modules/**', '!dist', '!dist/**'])
+    .pipe(zip(filename))
+    .pipe(gulp.dest(targetDir))
+})
